d68c3c9736cc508ca1d7d31a5710088772e9c2dd
wilgero <wil.gero.2020@gmail.com>
2022-06-17 00:08:16 -0400

    Se implemento el Modelo Catecumeno y la migracion para crear la tabla catecumenos

---
+++ database/migrations/2022_06_17_033816_create_catecumenos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catecumenos', function (Blueprint $table) {
+            $table->id();//Integer unsigned increment
+            $table->string('name');//Nombres
+            $table->string('surname');//Apellidos
+            $table->date('birth');//Fecha de nacimiento
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catecumenos');
+    }
+};
